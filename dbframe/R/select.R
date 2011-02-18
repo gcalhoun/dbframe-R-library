14f884ed57ce359e74eb4cc1cce5b49af04a354f
Gray Calhoun <gcalhoun@iastate.edu>
2011-02-18 10:11:38 -0600

    changed select to do compound queries

--- dbframe/R/select.R
+++ dbframe/R/select.R
@@ -1,17 +1,17 @@
-setGeneric("select", function(x,...) standardGeneric("select"))
+setGeneric("select", function(db, cols, ...) standardGeneric("select"))
 
-selectSQL <- function(tablesql, select = "*", where = NULL, group.by = NULL,
+selectSQL <- function(tablesql, cols = "*", where = NULL, group.by = NULL,
                       having = NULL, order.by = NULL, limit = NULL, ...) {
-  if ((length(select) == 1) && (nchar(select) == 0)) select <- NULL
-  select <- if (is.null(group.by)) {
-    paste(select, collapse = ", ")
+  if ((length(cols) == 1) && (nchar(cols) == 0)) cols <- NULL
+  cols <- if (is.null(group.by)) {
+    paste(cols, collapse = ", ")
   } else {
     ## add elements of the 'group.by' vector that aren't already in
-    ## select to the select statement.  The sapply part looks for
+    ## cols to the select statement.  The sapply part looks for
     ## group.by elements that are at the beginning of a select
     ## statement, so that it recognizes things like:
-    ## select = "m.s as THEs", group.by = "m.s"
-    paste(c(select, group.by[!sapply(group.by, function(g) g %in% substr(select, 1, nchar(g)))]),
+    ## cols = "m.s as THEs", group.by = "m.s"
+    paste(c(cols, group.by[!sapply(group.by, function(g) g %in% substr(cols, 1, nchar(g)))]),
           collapse = ", ")
   }
   group.by <- if (is.null(group.by)) "" else {
@@ -25,8 +25,63 @@ selectSQL <- function(tablesql, select = "*", where = NULL, group.by = NULL,
   where    <- if (is.null(where))    "" else paste("where", where)
   limit    <- if (is.null(limit))    "" else paste("limit", limit)
 
-  paste("select", select, "from", tablesql, where, group.by, having, order.by, limit)
+  paste("select", cols, "from", tablesql, where, group.by, having, order.by, limit)
 }
 
-setMethod("select", "dbframe",   function(x, ...) dbGetQuery(db(x), selectSQL(sql(x),...)))
-setMethod("select", "character", function(x, db,...) dbGetQuery(db, selectSQL(x, ...)))
+setMethod("select", signature = c("dbframe", "missing"), function(db, cols,...)
+          dbGetQuery(db(db), selectSQL(sql(db),...)))
+
+setMethod("select", signature = c("dbframe", "character"), function(db, cols,...)
+          dbGetQuery(db(db), selectSQL(sql(db), cols, ...)))
+
+setMethod("select", signature = c("list", "character"), function(db, cols, join,...)
+          stop("Joins are not yet implemented."))
+
+setMethod("select", signature = c("dbframe", "list"), function(db, cols,...) {
+  if (length(cols) == 1) return(select(db, cols[[1]],...))
+  
+  allargs <- list(...)
+  if ("comp" %in% names(allargs)) {
+    comp <- allargs[["comp"]]
+    allargs[["comp"]] <- NULL
+  } else {
+    comp <- "union all"
+  }
+  if (length(comp) == 1) {
+    comp <- rep(comp, length(cols) - 1)
+  } else if (length(comp) != length(cols) - 1) {
+    stop("'comp' must have one element or one fewer element than 'cols'")
+  }
+    
+  if ("order.by" %in% names(allargs)) {
+    ordertext <- paste(" order by ", paste(allargs[["order.by"]], collapse = ", "))
+    allargs[["order.by"]] <- NULL
+  } else {
+    ordertext <- ""
+  }
+
+  ## if 'limit' has more than one entry, we'll apply it to each of
+  ## the individual queries, otherwise we apply it at the end.
+  eachlimit <- vector("list", length(cols))
+  limtext <- ""
+  if ("limit" %in% names(allargs)) {
+    if (length(allargs[["limit"]]) > 1) {
+      if (length(allargs[["limit"]]) != length(cols)) stop("'limit' must have the same length as 'cols' or length 1")
+      eachlimit <- as.list(allargs[["limit"]])
+    } else {
+      limtext <- paste(" limit ", allargs[["limit"]])
+    }
+    allargs[["limit"]] <- NULL
+  }
+  
+  if (!all(sapply(cols, is.character)))
+    stop("All elements of 'cols' must be characters")
+
+  sqls <- sapply(seq_along(cols), function(j) {
+    allargs$tablesql <- sql(db)
+    allargs$cols <- cols[[j]]
+    allargs$limit <- eachlimit[[j]]
+    do.call(selectSQL, allargs)
+  })
+  dbGetQuery(db(db), paste(paste(sqls, c(comp, ""), collapse = " "), ordertext, limtext))
+})
