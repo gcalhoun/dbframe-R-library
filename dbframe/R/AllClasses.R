32645a4cb5141e47d45a5cb5077551d62d5b0367
Gray Calhoun <gcalhoun@iastate.edu>
2011-03-10 11:53:08 -0600

    added comparison methods for dbframe

--- dbframe/R/AllClasses.R
+++ dbframe/R/AllClasses.R
@@ -1,3 +1,8 @@
 setClass("dbframe", representation(db = "character",
                                    sql = "character",
                                    extensions = "logical"))
+
+setMethod("==", c("dbframe", "dbframe"), function(d1, d2)
+          d1@db == d2@db & d1@sql == d2@sql & d1@extensions == d2@extensions)
+setMethod("!=", c("dbframe", "dbframe"), function(d1, d2)
+          d1@db != d2@db & d1@sql != d2@sql & d1@extensions != d2@extensions)
