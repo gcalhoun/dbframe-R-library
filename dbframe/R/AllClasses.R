13675c149b26dca02867cbca7cc3b7bd656a5be2
Gray Calhoun <gcalhoun@iastate.edu>
2011-03-10 12:26:42 -0600

    added comparison operators

--- dbframe/R/AllClasses.R
+++ dbframe/R/AllClasses.R
@@ -2,7 +2,7 @@ setClass("dbframe", representation(db = "character",
                                    sql = "character",
                                    extensions = "logical"))
 
-setMethod("==", c("dbframe", "dbframe"), function(d1, d2)
-          d1@db == d2@db & d1@sql == d2@sql & d1@extensions == d2@extensions)
-setMethod("!=", c("dbframe", "dbframe"), function(d1, d2)
-          d1@db != d2@db & d1@sql != d2@sql & d1@extensions != d2@extensions)
+setMethod("==", c("dbframe", "dbframe"), function(e1, e2)
+          e1@db == e2@db & e1@sql == e2@sql & e1@extensions == e2@extensions)
+setMethod("!=", c("dbframe", "dbframe"), function(e1, e2)
+          e1@db != e2@db & e1@sql != e2@sql & e1@extensions != e2@extensions)
