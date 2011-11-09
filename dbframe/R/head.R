a9084583f0becd2864017ce796305133d40197d7
Gray Calhoun <gcalhoun@iastate.edu>
2011-11-09 02:39:33 -0600

    still moving stuff over

--- dbframe/R/head.R
+++
@@ -1,8 +0,0 @@
-head.dbframe <- function(x, n = 6L, ...) {
-  if (n >= 0) {
-    select(x, limit = n,...)
-  } else {
-    nrec <- select(x, "count(*)")[[1]]
-    select(x, limit = nrec + n,...)
-  }
-}
--- dbframe/R/tail.R
+++
@@ -1,8 +0,0 @@
-tail.dbframe <- function(x, n = 6L, ...) {
-  nrec <- select(x, "count(*)")[[1]]
-  if (n >= 0) {
-    select(x, limit = sprintf("%d,%d", nrec - n, n),...)
-  } else {
-    select(x, limit = sprintf("%d,%d", -n, nrec + n),...)
-  }
-}
