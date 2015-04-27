00f038ecba08ac3f73331b9ef5043a22e73bfe28
Gray Calhoun <gray@clhn.org>
2015-04-27 03:46:22 -0500

    Tweak arguments for 'clear' for debugging

--- R/clear.R
+++ R/clear.R
@@ -1,19 +1,17 @@
 ## Copyright (C) 2010-2015 Gray Calhoun; MIT license
 
-    setGeneric("clear", function(...)
-               standardGeneric("clear"), signature = "...")
+setGeneric("clear", function(...) standardGeneric("clear"), signature = "...")
 
-    setMethod("clear", signature = "dbframe", function(...) {
-      x <- list(...)
-      sapply(x, function(y) {
+setMethod("clear", signature = "dbframe", function(...) {
+    x <- list(...)
+    sapply(x, function(y) {
         stopifnot(!readonly(y))
         dbc <- dbConnect(y)
-        results <- res <- 
-                     if (is.linked(y)) {
-                       dbRemoveTable(dbc, tablename(y),...)
-                     } else {
-                       FALSE
-                     }
+        results <- if (is.linked(y)) {
+            dbRemoveTable(dbc, tablename(y))
+        } else {
+            FALSE
+        }
         dbDisconnect(dbc)
         results
-      })})
+    })})
